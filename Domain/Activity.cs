93a96343eb6e9444d5e5ef336ac3504adb327570
callmecelsius <celsius.mp@gmail.com>
2026-02-22 22:48:41 -0600

    - updated entity to include nonnullable columns

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -4,20 +4,20 @@ public class Activity
 {
 	public string Id { get; set; } = Guid.NewGuid().ToString();
 
-	public string? Title { get; set; }
+	public required string? Title { get; set; }
 
 	public DateTime Date { get; set; }
 
-	public string Description { get; set; } = string.Empty;
+	public required string Description { get; set; } = string.Empty;
 
-	public string Category { get; set; } = string.Empty;
+	public required string Category { get; set; } = string.Empty;
 
 	public bool IsCancelled { get; set; }
 
 	// location props
-	public string City { get; set; } = string.Empty;
+	public required string City { get; set; } = string.Empty;
 
-	public string Venue { get; set; } = string.Empty;
+	public required string Venue { get; set; } = string.Empty;
 
 	public double Latitude { get; set; }
 }
