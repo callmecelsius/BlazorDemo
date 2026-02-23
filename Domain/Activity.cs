d9b44feca17bece2652b5eb0e6ba801fb9f395b8
callmecelsius <celsius.mp@gmail.com>
2026-02-22 22:46:10 -0600

    - initial commit

---
+++ Domain/Activity.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain;
+
+public class Activity
+{
+	public string Id { get; set; } = Guid.NewGuid().ToString();
+
+	public string? Title { get; set; }
+
+	public DateTime Date { get; set; }
+
+	public string Description { get; set; } = string.Empty;
+
+	public string Category { get; set; } = string.Empty;
+
+	public bool IsCancelled { get; set; }
+
+	// location props
+	public string City { get; set; } = string.Empty;
+
+	public string Venue { get; set; } = string.Empty;
+
+	public double Latitude { get; set; }
+}
